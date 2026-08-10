318cd84100bdd71227e9da2327b6d23d618f7a68
Seoha101109 <122782179+Seoha101109@users.noreply.github.com>
2026-08-10 19:50:17 +0900

    대체 텍스트 추가

--- calender_service.py
+++ calender_service.py
@@ -185,7 +185,7 @@ def make_event_body(notice: dict):
         <br><br>
         <hr>
         <p><b>Teams 원본 메시지 바로가기:</b><br>
-        <a href="{web_url}" target="_blank">{web_url}</a>
+        <a href="{web_url}" target="_blank">Microsoft Teams</a>
         </p>
         """
 
