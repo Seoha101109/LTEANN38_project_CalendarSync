b93fc748b54ef3fcbd2e97aee02eb8638ae97a62
Seoha101109 <122782179+ikiterujan@users.noreply.github.com>
2026-08-11 08:18:03 +0900

    들여쓰기 삭제

--- calender_service.py
+++ calender_service.py
@@ -182,13 +182,7 @@ def make_event_body(notice: dict):
     if web_url:
         logger.info(f"🎯 [Calendar Service] web_url 정상 수신: {web_url}")
         # 본문 하단에 원본 메시지 링크 HTML 생성
-        content += f"""
-        <br><br>
-        <hr>
-        <p><b>Teams 원본 메시지 바로가기:</b><br>
-        <a href="{html.escape(str(web_url))}" target="_blank">Microsoft Teams</a>
-        </p>
-        """
+        content += f"""<br><br><hr><p><b>Teams 원본 메시지 바로가기:</b><br><a href="{html.escape(str(web_url))}" target="_blank">Microsoft Teams</a></p>"""
 
     # 6. MS Graph API Payload 생성
     event = {
