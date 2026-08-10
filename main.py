26a83cbadecd25ad7eb5c765608a6d39e86d0f0f
Seoha101109 <122782179+ikiterujan@users.noreply.github.com>
2026-08-10 21:11:36 +0900

    0부터 시작한다니까

--- main.py
+++ main.py
@@ -682,7 +682,7 @@ async def auto_polling_sync_job():
         group_index = (now_utc.minute // POLLING_INTERVAL_MINUTES) % POLLS_PER_HOUR
         target_users = [
             user for idx, user in enumerate(all_users)
-            if idx == 6
+            if idx == 5
             #if idx % POLLS_PER_HOUR == group_index
         ]
 
